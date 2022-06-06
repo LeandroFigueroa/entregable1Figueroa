9545e50ea90da92a7697879ff45d446c1f3b4f62
Leandro Figueroa <105681474+LeandroFigueroa@users.noreply.github.com>
2022-06-06 18:39:02 -0300

    render html

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse    
+
 # Create your views here.
 
 def index (request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render (request, "webapp/index.html")
 
+def estudiantes (request):
+    return render(request, 'webapp/estudiantes.html')
 
-    
+def profesores (request):
+    return render(request, 'webapp/profesores.html')
+
